b3f7f78a1d3c83e2d496381a7016f8bc9205abc3
“wj” <1851823350@qq.com>
2022-10-03 12:10:14 +0800

    医院模拟系统

---
+++ hospital-manage/src/main/java/com/atwj/hospital/mapper/HospitalSetMapper.java
@@ -0,0 +1,10 @@
+package com.atwj.hospital.mapper;
+
+import com.atguigu.hospital.model.HospitalSet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
