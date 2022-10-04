b057e6df7b8c0c330c53c5da37aef12b36ac5aa4
“wj” <1851823350@qq.com>
2022-10-04 15:04:37 +0800

    数据字典

--- service/service_cmn/src/main/java/com/atwj/yygh/ServiceCmnMain8202.java
+++ service/service_cmn/src/main/java/com/atwj/yygh/ServiceCmnMain8202.java
@@ -3,6 +3,7 @@ package com.atwj.yygh;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.atwj")
 @MapperScan("com.atwj.yygh.mapper")
+@EnableDiscoveryClient
 public class ServiceCmnMain8202 {
     public static void main(String[] args){
         SpringApplication.run(ServiceCmnMain8202.class, args);
